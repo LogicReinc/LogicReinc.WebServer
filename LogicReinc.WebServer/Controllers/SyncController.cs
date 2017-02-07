bdcdf8b3ad268ceac25d5ce52feb7ac7d882a8d4
Kelvin K <logicincarnate@gmail.com>
2017-02-07 17:02:28 +0100

    Added recursion resolve

--- LogicReinc.WebServer/Controllers/SyncController.cs
+++ LogicReinc.WebServer/Controllers/SyncController.cs
@@ -139,15 +139,15 @@ namespace LogicReinc.WebSercer.Controllers
                         jsControllers.AddObject(template.ControllerType.Name.Replace("Controller", "").Replace("`1", ""), jsController);
                 }
 
-                JSBuilder js = new JSBuilder()
-                    .AssignVariable("Sync", jsControllers);
+                JSBuilder js = new JSBuilder().AssignVariable("Sync", jsControllers);
 
-                if(!isUpdate)
-                js.DefineFunction("SyncGetJson", @"
+                if (!isUpdate)
+                    js.DefineFunction("SyncGetJson", @"
     var ajax = new XMLHttpRequest();
     ajax.onreadystatechange = function () {
         if (ajax.readyState == 4 && ajax.status == 200)
-            callback(JSON.parse(ajax.responseText))
+            /*callback(JSON.parse(ajax.responseText))*/
+            callback(ResolveReferences(ajax.responseText))
     };
     ajax.open('GET', url, true);
     ajax.setRequestHeader('Accept', 'application/json');
@@ -157,12 +157,58 @@ namespace LogicReinc.WebSercer.Controllers
     var ajax = new XMLHttpRequest();
     ajax.onreadystatechange = function () {
         if (ajax.readyState == 4 && ajax.status == 200) {
-            callback(JSON.parse(ajax.responseText))
+            /*callback(JSON.parse(ajax.responseText))*/
+            callback(ResolveReferences(ajax.responseText))
         }
     };
     ajax.open('POST', url, true);
     ajax.send(data);
-", "url", "data", "callback");
+", "url", "data", "callback")
+                    .DefineFunction("ResolveReferences", @"if (typeof json === 'string')
+        json = JSON.parse(json);
+
+    var byid = {}, // all objects by id
+        refs = []; // references to objects that could not be resolved
+    json = (function recurse(obj, prop, parent) {
+        if (typeof obj !== 'object' || !obj) // a primitive value
+            return obj;
+        if (Object.prototype.toString.call(obj) === '[object Array]') {
+            for (var i = 0; i < obj.length; i++)
+                // check also if the array element is not a primitive value
+                if (typeof obj[i] !== 'object' || !obj[i]) // a primitive value
+                    continue;
+                else if (""$ref "" in obj[i])
+                    obj[i] = recurse(obj[i], i, obj);
+                else
+                    obj[i] = recurse(obj[i], prop, obj);
+                return obj;
+            }
+            if (""$ref"" in obj) { // a reference
+                var ref = obj.$ref;
+                if (ref in byid)
+                return byid[ref];
+                // else we have to make it lazy:
+                refs.push([parent, prop, ref]);
+                return;
+            } else if (""$id"" in obj) {
+                var id = obj.$id;
+                delete obj.$id;
+                if (""$values"" in obj) // an array
+                obj = obj.$values.map(recurse);
+            else // a plain object
+                for (var prop in obj)
+                    obj[prop] = recurse(obj[prop], prop, obj);
+                byid[id] = obj;
+            }
+            return obj;
+        })(json); // run it!
+
+    for (var i = 0; i<refs.length; i++) { // resolve previously unknown references
+        var ref = refs[i];
+        ref[0][ref[1]] = byid[ref[2]];
+        // Notice that this throws if you put in a reference at top-level
+    }
+    return json;", "json");
 
 
                 if (Authenticated)
